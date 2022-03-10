84b59b24236ffa62af65f49d05afddae22df5882
Dyether <dyether.lugo@gmail.com>
2022-03-09 21:38:36 -0600

    primer commit

---
+++ lab1progra2Dyether.py
@@ -0,0 +1,29 @@
+from pickle import TRUE
+
+while TRUE:
+    opcion=input("digite una opcion: " )
+    if(not(opcion.isdigit())):
+        print("debe dijitar una opcion entre 1 y 5: ")
+        continue
+    i = int(opcion) 
+    if i == 1:
+            horas = int(input("¿Cuantas horas trabajaste?:"))
+            resultado = horas * 1600
+            print("Tu salario actual es de: " + str(resultado))
+    elif i == 2:
+        nombre = input("Cual es tu nombre?: ")
+        print("Bienvenido a la UIA " + nombre.upper())
+        print("Bienvenido a la UIA " + nombre.lower())    
+    elif i == 3:
+        edad = int(input("Cuale es tu edad?: "))
+        if (edad >= 18 ):
+            print("Eres mayor de edad")
+        else:
+            print("Eres menor de edad")
+    elif i == 4:
+        numero = int(input("Digita un munero entero entre uno y cien: ")) 
+        for a in range (1, numero + 1, 2):
+            print (a, end = ",")
+    elif i == 5:
+        print("Gracias por acceder.")
+        break
